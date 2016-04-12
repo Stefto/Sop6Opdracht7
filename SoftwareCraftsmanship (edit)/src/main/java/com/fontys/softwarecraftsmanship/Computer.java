d4faf97a8d38c26f889f960498bcbe60762665b2
Stefan <steftonia@gmail.com>
2016-04-12 18:53:05 +0200

    Initial commit

---
+++ SoftwareCraftsmanship (edit)/src/main/java/com/fontys/softwarecraftsmanship/Computer.java
@@ -0,0 +1,63 @@
+package com.fontys.softwarecraftsmanship;
+
+import java.util.*;
+
+public class Computer {
+
+    List<Part> parts;
+
+    public Computer(){
+        parts = new ArrayList<>();
+    }
+    
+    public Computer(List<Part> parts){
+        this.parts = parts;
+    }
+    
+    public int NumberOfParts() {
+        return parts.size() - 1;
+    }
+
+    public void AddPart(Part p) {
+        parts.add(p);
+    }
+
+    public double GetPrice() {
+        double price = 0.0;
+
+        for (Part part : parts) {
+            price += part.GetPrice();
+        }
+
+        return price;
+    }
+
+    public boolean IsComplete() {
+        boolean hasCasing = false;
+        boolean hasProcessor = false;
+        boolean hasMemory = false;
+        boolean hasMotherboard = false;
+        
+        for (Part part : parts) {
+            switch (part.GetType()) {
+                case Casing:
+                    hasCasing = true;
+                    break;
+                case Processor:
+                    hasProcessor = true;
+                    break;
+                case Memory:
+                    hasMemory = true;
+                    break;
+                case Motherboard:
+                    hasMotherboard = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        //not zo heel erg elegant, maar sta open voor suggesties
+        return !(!hasCasing || !hasProcessor || !hasMemory || !hasMotherboard);
+    }
+}
---
+++ SoftwareCraftsmanship (edit)/src/main/java/com/fontys/softwarecraftsmanship/Part.java
@@ -0,0 +1,51 @@
+package com.fontys.softwarecraftsmanship;
+
+public class Part {
+    
+    private final PartType type;
+    private final String title;
+    private final double price;
+    private String subtype = null;
+    private int size = -1;
+    private double speed = -1.0;
+    
+    public Part(PartType type, String title, double price, String subtype, int size, double speed) {
+        this(type,title,price,subtype,size);
+        this.speed = speed;
+    }
+    
+    // Memory
+    public Part(PartType type, String title, double price, String subtype, int size) {
+        this(type,title,price,subtype);
+        this.size = size;
+    }
+    
+    // Casing
+    public Part(PartType type, String title, double price, String subtype) {
+        this(type,title,price);
+        this.subtype = subtype;
+    }
+    
+    // Processor
+    public Part(PartType type, String title, double price, double speed) {
+        this.type = type;
+        this.title = title;
+        this.price = price;
+        this.speed = speed;
+    }
+    
+    // Motherboard
+    public Part(PartType type, String title, double price) {
+        this.type = type;
+        this.title = title;
+        this.price = price;
+    }
+    
+    public PartType GetType() {
+        return type;
+    }
+    
+    public double GetPrice() {
+        return price;
+    }
+}
